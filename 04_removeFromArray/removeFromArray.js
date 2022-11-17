62f135e977598c4cc54dead793d6c2a3428700e2
ed benedict quia <edbenedictquia@gmail.com>
2022-11-17 22:13:14 +0800

    pass all test of 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+let wholeArrayLength = arguments.length;
+let firstArrayLength = arguments[0].length;
+  for(let i = 1; i < wholeArrayLength; i++){
+    for(let a = 0; a < firstArrayLength; a++){
+      if(arguments[0][a] === arguments[i]){
+        arguments[0].splice(a,1);
+      }
+    }
+  }
+return arguments[0];
 };
 
 // Do not edit below this line
