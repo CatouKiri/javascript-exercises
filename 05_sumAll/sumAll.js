8a9cacfdcba4c4531bfd8e9eae2153a4f7abdd90
ed benedict quia <edbenedictquia@gmail.com>
2022-11-17 22:47:45 +0800

    pass the first two test of 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
 const sumAll = function() {
-
+let min = arguments[0];
+let max = arguments[1];
+let a = min;
+while(min < max){
+    a = a + (min + 1);
+    min++;
+}
+return a;
 };
 
 // Do not edit below this line
