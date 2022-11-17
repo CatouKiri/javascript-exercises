be4111cbf9edd85da93e5fb6524d43128822463b
ed benedict quia <edbenedictquia@gmail.com>
2022-11-17 20:41:17 +0800

    pass the test of 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(string === '' || num === 0){
+        string = '';
+        num = 0;
+    }
+    else if(num > 0){
+        for (let i = 1; i < num; i++) {
+            string += 'hey';
+        }
+    }
+    else if(num < 0){
+        string = 'ERROR';
+    }
+    return string;
 };
 
 // Do not edit below this line
