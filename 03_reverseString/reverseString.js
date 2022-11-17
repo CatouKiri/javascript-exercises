d8ce71e9395d5028f9556c93ebfaed3f96430f86
ed benedict quia <edbenedictquia@gmail.com>
2022-11-17 21:00:48 +0800

    pass test of 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let array = string.split("");
+array = array.reverse();
+array = array.join("");
+return array.toString();
 };
 
 // Do not edit below this line
