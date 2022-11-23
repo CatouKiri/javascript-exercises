091004780404be9cf58530cb0c5b8dd64125924a
ed benedict quia <edbenedictquia@gmail.com>
2022-11-23 22:11:31 +0800

    pass all test of 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(given) {
+    if(given % 4 === 0 && String(given / 4).slice(-1) != 5){
+      return true;
+    }
+    else{
+      return false;
+    }
 };
 
 // Do not edit below this line
