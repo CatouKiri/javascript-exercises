483b2b8ba7a7a7d55718f1870f482ce297267c23
ed benedict quia <edbenedictquia@gmail.com>
2022-11-23 22:25:26 +0800

    pass all test of 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celcius = (fahrenheit - 32) * (5 / 9)
+  if (celcius % 1 === 0){
+    return celcius;
+  }
+  else{
+    return (celcius).toFixed(1) * 1;
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let fahrenheit = (celcius * (9 / 5)) + 32; 
+  if (fahrenheit % 1 === 0){
+    return fahrenheit;
+  }
+  else{
+    return (fahrenheit).toFixed(1) * 1;
+  }
 };
 
 // Do not edit below this line
