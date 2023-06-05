aa469a255a20f119c3a3c543a41eaef3f27c892c
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-06-05 21:12:27 +0800

    solve exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,53 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // let currentOldest = 0;
+    // let a = -1;
+    // for(let i = 0; i < people.length; i++){
+    //     let death = people[i].yearOfDeath;
+    //     let birth = people[i].yearOfBirth;
+    //     if(people[i].yearOfDeath === undefined){
+    //         const d = new Date();
+    //         death = d.getFullYear();
+    //     }
+    //     let currentAge = death - birth;
+    //     if(currentOldest < currentAge){
+    //         currentOldest = currentAge;
+    //     }
+    //     else{
+    //         a++;
+    //     }
 
+    // }
+    // return people[a];
+
+
+    // declare c = 0 for array item oldest
+    let c = 0;
+    for(let i = 0; i < people.length-1; i++){
+        // declare death
+        let death1 = people[i].yearOfDeath;
+        let death2 = people[i+1].yearOfDeath;
+        // if persons death is not defined use current year
+        if(death1 === undefined){
+            const d = new Date();
+            death1 = d.getFullYear();
+        }
+        else if(death2 === undefined){
+            const d = new Date();
+            death2 = d.getFullYear();
+        }
+        // first age - person a = people[0]
+        age1 = death1 - people[i].yearOfBirth;
+        // second age - person b = people[1]
+        age2 = death2 - people[i+1].yearOfBirth
+        // compare first and second person
+        if(age1 <  age2){
+        // if second person is oldest c++
+        c++;
+        }
+        // if first person is oldest - no change
+    }
+
+    return people[c];
 };
 
 // Do not edit below this line
