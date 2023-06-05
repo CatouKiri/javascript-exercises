84dfd818f46107ed58e9f162ceec8142b1781ae4
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-06-05 19:00:03 +0800

    solve the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0) {
+        return "OOPS"
+    }
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    let i = 1;
+    for (i = 1; i < num; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    return num2;
 };
 
 // Do not edit below this line
